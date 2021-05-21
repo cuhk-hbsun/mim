3e8e38a320785966a3f74f53cbed5c81abe0b67d
sunhongbin <sunhongbin@sensetime.com>
2021-05-21 21:22:24 +0800

    just test

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 project = 'mim'
 copyright = '2021, openmmlab'
-author = 'openmmlab'
+author = 'MIM authors'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
@@ -35,9 +35,8 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinxcontrib.napoleon',
-    'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode',
+    'recommonmark', 'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -54,9 +53,10 @@ pygments_style = 'sphinx'
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
+html_static_path = []
